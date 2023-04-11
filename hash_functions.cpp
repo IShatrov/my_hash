82284f247079764d5babc0a94122281e3249245c
IShatrov <shatrov2004@mail.ru>
2023-04-11 20:38:45 +0300

    my_rotl and my_rotr

--- hash_functions.cpp
+++ hash_functions.cpp
@@ -1,11 +1,27 @@
 #include "my_hash.h"
 
+const unsigned int bits_8 = 8;
+
 int assert_hash_arg(const char* key, int size)
 {
     assert(key);
     assert(size);
 
     return 1;
+}
+
+unsigned char my_rotl(unsigned char num, unsigned char count)
+{
+    unsigned int r = count % bits_8;
+
+    return (num << r) | (num >> (bits_8 - r));
+}
+
+unsigned char my_rotr(unsigned char num, unsigned char count)
+{
+    unsigned int r = count % bits_8;
+
+    return (num >> r) | (num << (bits_8 - r));
 }
 
 int hash_const(const char* key, int size)
--- hash_functions.h
+++ hash_functions.h
@@ -2,6 +2,10 @@
 
 int assert_hash_arg(const char* key, int size);
 
+unsigned char my_rotl(unsigned char num, unsigned char count);
+
+unsigned char my_rotr(unsigned char num, unsigned char count);
+
 int hash_const(const char* key, int size);
 
 int hash_first(const char* key, int size);
